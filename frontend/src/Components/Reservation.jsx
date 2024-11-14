7764e8e2135a9f15cdaa50e3f7bdf44bad116d1f
Sopan Bharkad <131639162+Sopan7654@users.noreply.github.com>
2024-11-14 22:33:54 +0530

    Update Reservation.jsx

--- frontend/src/Components/Reservation.jsx
+++ frontend/src/Components/Reservation.jsx
@@ -24,7 +24,7 @@ const Reservation = () => {
 
     try {
       const { data } = await axios.post(
-        "https://your-backend-url.onrender.com/api/reservations/send",
+        "https://localhost4000/api/reservations/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
